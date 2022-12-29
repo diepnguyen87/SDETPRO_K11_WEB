bdeaed1fe7f2967f23975ecf55d398a7a44ed772
Diep <diep.nguyen@MBP-0230s.local>
2022-12-29 17:31:39 +0700

    lesson 17, mouse hover + narrow down searching scope

--- src/test/java/url/Urls.java
+++ src/test/java/url/Urls.java
@@ -5,4 +5,5 @@ public interface Urls {
     String baseUrl = "https://the-internet.herokuapp.com";
     String dropdownSlug = "/dropdown";
     String iframeSlug = "/iframe";
+    String hoverSlug = "/hovers";
 }
